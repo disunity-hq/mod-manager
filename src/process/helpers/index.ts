d2849ad6926cc21acdce6904a059f62736f8a6e4
George Tsomlektsis <ultrawelfaredev@gmail.com>
2019-05-06 22:20:22 +0300

    Converted zip function to async

--- src/process/helpers/index.ts
+++ src/process/helpers/index.ts
@@ -1,30 +1,25 @@
-import axios from "axios";
 import { BrowserWindow, app } from "electron";
 import { download } from "electron-dl";
 import * as path from "path";
 import * as DecompressZip from "decompress-zip";
-import { unzip } from "zlib";
 
-export function extractOnlineZip(
-  url: string,
-  extractPath: string
-): Promise<any> {
+export const extractOnlineZip = async (url: string, extractPath: string) => {
   const focusedWindow: BrowserWindow = BrowserWindow.getFocusedWindow();
   const tempFolder: string = app.getPath("temp");
-  return new Promise((resolve, reject) => {
-    download(focusedWindow, url, {
+  try {
+    await download(focusedWindow, url, {
       directory: tempFolder,
       filename: "temp.zip"
-    })
-      .then(_ => {
-        const savedFile: string = path.join(tempFolder, "temp.zip");
-        const unzipper = new DecompressZip(savedFile);
-        unzipper.extract({
-          path: extractPath
-        });
-        unzipper.on("extract", () => resolve());
-        unzipper.on("error", (err: any) => reject(err));
-      })
-      .catch(err => reject(err));
-  });
-}
+    });
+    const savedFile: string = path.join(tempFolder, "temp.zip");
+    const unzipper = new DecompressZip(savedFile);
+
+    // decompress-zip doesn't have typescript definitions by default :(
+    unzipper.extract({
+      path: extractPath
+    });
+    unzipper.on("error", (e: any) => { throw new Error(e); });
+  } catch (e) {
+    throw new Error(e);
+  }
+};
