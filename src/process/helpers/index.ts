c383c55cc5924f47efa3a90a8c1ca165c1e29f0b
George Tsomlektsis <ultrawelfaredev@gmail.com>
2019-05-06 23:50:48 +0300

    Moved helper function to class

--- src/process/helpers/index.ts
+++ src/process/helpers/index.ts
@@ -3,23 +3,27 @@ import { download } from "electron-dl";
 import * as path from "path";
 import * as DecompressZip from "decompress-zip";
 
-export const extractOnlineZip = async (url: string, extractPath: string) => {
-  const focusedWindow: BrowserWindow = BrowserWindow.getFocusedWindow();
-  const tempFolder: string = app.getPath("temp");
-  try {
-    await download(focusedWindow, url, {
-      directory: tempFolder,
-      filename: "temp.zip"
-    });
-    const savedFile: string = path.join(tempFolder, "temp.zip");
-    const unzipper = new DecompressZip(savedFile);
+export default class Helpers {
+  static async extractOnlineZip(url: string, extractPath: string) {
+    const focusedWindow: BrowserWindow = BrowserWindow.getFocusedWindow();
+    const tempFolder: string = app.getPath("temp");
+    try {
+      await download(focusedWindow, url, {
+        directory: tempFolder,
+        filename: "temp.zip"
+      });
+      const savedFile: string = path.join(tempFolder, "temp.zip");
+      const unzipper = new DecompressZip(savedFile);
 
-    // decompress-zip doesn't have typescript definitions by default :(
-    unzipper.extract({
-      path: extractPath
-    });
-    unzipper.on("error", (e: any) => { throw new Error(e); });
-  } catch (e) {
-    throw new Error(e);
+      // decompress-zip doesn't have typescript definitions by default :(
+      unzipper.extract({
+        path: extractPath
+      });
+      unzipper.on("error", (err: any) => {
+        throw new Error(err);
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
   }
-};
+}
