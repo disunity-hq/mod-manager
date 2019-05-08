bc8b776c04425b870cf57db60145ed20a46df969
George Tsomlektsis <ultrawelfaredev@gmail.com>
2019-05-09 02:32:18 +0300

    Fixed storybook not loading antd css file

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import "antd/dist/antd.css";
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../src/stories/', true, /\.stories\.tsx$/);
 
