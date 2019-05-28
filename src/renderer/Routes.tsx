a12d251609456bec69c3ee9a78434c1452d95150
Scott Techau <scottbot95@gmail.com>
2019-05-28 14:21:36 -0700

    Add basic route and page for browsing games

--- src/renderer/Routes.tsx
+++ src/renderer/Routes.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import TestPage from './components/pages/TestPage';
+import AddGamePage from './components/pages/AddGame/AddGame';
 import PackagePage from './components/pages/PackagePage/PackagePage';
 
 export default class Routes extends React.Component {
   public render(): React.ReactNode {
     return (
       <Switch>
-        <Redirect path="/" exact={true} to="/games" />
-        <Route path="/games" component={PackagePage} />
+        <Redirect path="/" exact to="/games" />
+        <Redirect path="/games" exact to="/games/browse" />
+        <Route path="/games/browse" component={AddGamePage} />
+        <Route path="/games/:game" component={PackagePage} />
         <Route path="/settings" component={TestPage} />
       </Switch>
     );
--- src/renderer/components/pages/AddGame/AddGame.tsx
+++ src/renderer/components/pages/AddGame/AddGame.tsx
@@ -1,4 +1,14 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
 
-const AddGame = (): React.ReactElement => <Layout />;
+const { Content } = Layout;
+
+const AddGame = (): React.ReactElement => (
+  <Layout>
+    <Content>
+      <Typography.Title>Browse For Games</Typography.Title>
+    </Content>
+  </Layout>
+);
+
+export default AddGame;
